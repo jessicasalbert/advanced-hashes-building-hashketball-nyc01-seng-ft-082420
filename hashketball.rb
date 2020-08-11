9a32380230719380128d9aa4f42e4e1d2fe718dd
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-11 13:36:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,7 +54,7 @@ def game_hash
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: [
-        { player_name: "Jess Adrien" ,
+        { player_name: "Jeff Adrien" ,
         number: 4,
         shoe: 18,
         points: 10 ,
